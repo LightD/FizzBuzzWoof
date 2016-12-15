abe12f4ebd768861eb3e8590825eeaf6de7acba8
Light Dream <nourforgive@gmail.com>
2016-12-16 01:26:58 +0800

    Initial Commit

---
+++ fizzbuzzwoof/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fizzbuzzwoof
+//
+//  Created by Light Dream on 16/12/2016.
+//  Copyright © 2016 kfit. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
